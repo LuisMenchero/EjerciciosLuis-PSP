43246b1274e5aa59d5d5d2fd8af2fdbdd6d8ed26
LuisMenchero <luismencheroperezcejuela@gmail.com>
2026-09-24 13:12:51 +0200

    ejercicio 2 hecho

--- Ejs_UT1/fork02.c
+++ Ejs_UT1/fork02.c
@@ -1,1 +1,21 @@
 // Segundo ejercicio
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main() {
+    pid_t pid, pid_hijo;
+
+    pid = fork();
+
+    if (pid == 0) {
+        printf("Luis \n");
+    } else {
+        pid_hijo = wait(NULL);
+        printf("Mi hijo tiene PID es %d \n",pid_hijo);
+        printf("Mi PID es %d \n",getpid());
+    }
+   exit(0);
+}
